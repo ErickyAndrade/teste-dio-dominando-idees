61fa3b7a878917a35417de242d5fed945b8cedff
Ericky <erickydeandradrdavi@gmail.com>
2022-07-20 23:30:22 -0300

    criando class Livro

--- Intellij/primeiro programa/src/br/com/dio/PrimeiroProgramaIntellij.java
+++ Intellij/primeiro programa/src/br/com/dio/PrimeiroProgramaIntellij.java
@@ -2,16 +2,38 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
-public class PrimeiroProgramaIntellij {
-      public static void main(String[] args) {
-          Gato gato = new Gato();
-          System.out.println(gato);
 
+class Livro {
+    private String nome;
+    private Integer numPaginas;
 
-        /*int a = 5;
-        int b = 3;
-        System.out.println("Hello World " + (a+b));*/
-      }
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
-
 
